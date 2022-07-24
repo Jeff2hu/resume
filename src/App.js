3f1899f7c4e7f942f0993061b997dc3e01da71ff
Jeff2hu <lovezhu661991@gmail.com>
2022-07-24 16:43:31 +0800

    project

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
       <Header />
       <Nav />
       <Routes>
-        <Route path='/resume' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/experience' element={<Experience />} />
-        <Route path='/project' element={<Project />} />
+        <Route path='/resume/' element={<About />} />
+        <Route path='/contact/' element={<Contact />} />
+        <Route path='/experience/' element={<Experience />} />
+        <Route path='/project/' element={<Project />} />
       </Routes>
       <Footer />
     </div>
